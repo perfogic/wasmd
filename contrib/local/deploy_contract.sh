c56f77f83fe9b04b269e57a8c6b48993440c6700
Pham Tu <tubackkhoa@gmail.com>
2022-09-28 06:51:34 +0700

    update contrib

--- contrib/local/deploy_contract.sh
+++ contrib/local/deploy_contract.sh
@@ -8,12 +8,10 @@ code_id=${4:-}
 CHAIN_ID=${CHAIN_ID:-testing}
 ADMIN=${ADMIN:-validator}
 
-echo "Enter passphrase:"
-read -s passphrase
 
 if [ -z $code_id ]
 then 
-    store_ret=$(echo $passphrase | wasmd tx wasm store $contract_path --from $ADMIN --gas="auto" --gas-adjustment 1.2 --chain-id=$CHAIN_ID --keyring-backend test -y)
+    store_ret=$(wasmd tx wasm store $contract_path --from $ADMIN --gas="auto" --gas-adjustment 1.2 --chain-id=$CHAIN_ID --keyring-backend test -y)
     echo $store_ret
     if [ ! `command -v jq` ]; then  
         echo "Installing jq ..."
@@ -25,9 +23,9 @@ fi
 # echo "wasmd tx wasm instantiate $code_id '$init' --from $ADMIN --label '$label' --gas auto --gas-adjustment 1.2 --keyring-backend test --chain-id=$CHAIN_ID -y"
 # quote string with "" with escape content inside which contains " characters
 
-admin=$(echo $passphrase | wasmd keys show $ADMIN --keyring-backend test --output json | jq -r '.address')
+admin=$(wasmd keys show $ADMIN --keyring-backend test --output json | jq -r '.address')
 
-(echo $passphrase;echo $passphrase) | wasmd tx wasm instantiate $code_id "$init" --from $ADMIN --label "$label" --gas auto --keyring-backend test --gas-adjustment 1.2 --admin $admin --chain-id=$CHAIN_ID -y
+wasmd tx wasm instantiate $code_id "$init" --from $ADMIN --label "$label" --gas auto --keyring-backend test --gas-adjustment 1.2 --admin $admin --chain-id=$CHAIN_ID -y
 contract_address=$(wasmd query wasm list-contract-by-code $code_id --output json | jq -r '.contracts[-1]')
 
 echo "contract address: $contract_address"
